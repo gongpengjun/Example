fb886990e659ecece7ef74f03ede19c5ad486c5a
gongpengjun <gongpengjun@didichuxing.com>
2017-09-21 11:05:03 +0800

    完善启动屏幕视图、导航栏颜色

--- main.m
+++ main.m
@@ -1,4 +1,5 @@
 #import <UIKit/UIKit.h>
+#import "MainViewController.h"
 
 int main(int argc, char *argv[])
 {
@@ -8,3 +9,29 @@ int main(int argc, char *argv[])
         return retVal;
     }
 }
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (nonatomic, strong) UIWindow *window;
+@property (strong, nonatomic) UINavigationController *navigationController;
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    {
+        MainViewController *mainViewController = [[MainViewController alloc] initWithNibName:nil bundle:nil];
+        mainViewController.edgesForExtendedLayout = UIRectEdgeNone;
+        
+        self.navigationController = [[UINavigationController alloc] initWithRootViewController:mainViewController];
+        self.navigationController.navigationBar.barStyle = UIBarStyleDefault;
+        self.navigationController.navigationBar.barTintColor = [UIColor whiteColor];
+    }
+    self.window.rootViewController = self.navigationController;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
+
